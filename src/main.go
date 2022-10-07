a3100c1aa8052f4988d5813ce2d219ff7b5ad6b4
gmatthews87 <graham@utiliz.app>
2022-10-07 13:06:16 -0500

    validate email and password

--- src/main.go
+++ src/main.go
@@ -5,51 +5,74 @@ import (
 	"log"
 	"net/http"
 	"encoding/json"
+	"fmt"
+	"errors"
 )
 
+// Woah, why is code a string? code is a four digit number that contains
+// 	the two digit hour and minute the request was generated. What happens
+//	when the hour has a leading zero, ex: 0908 for 9:08 AM? If made a
+//	number, the leading zero would be removed. So, let's make it a string
 type LoginRequest struct {
-	email string
-	password string
-	// Woah, why is code a string? code is a four digit number that contains
-	// 	the two digit hour and minute the request was generated. What happens
-	//	when the hour has a leading zero, ex: 0908 for 9:08 AM? If made a
-	//	number, the leading zero would be removed. So, let's make it a string
-	code string
-}
-
-func setStatus (rw http.ResponseWriter, req *http.Request, status int) {
-	switch status {
-	case 200:
-		rw.WriteHeader(http.StatusOK)
-	case 301:
-		rw.WriteHeader(http.StatusMovedPermanently)
-	case 400:
-		rw.WriteHeader(http.StatusBadRequest)
-	case 405:
-		rw.WriteHeader(http.StatusMethodNotAllowed)
-	}
+	Email string
+	Password string
+	Code string
 }
 
 func loginHandler (rw http.ResponseWriter, req *http.Request) {
-	if (req.Method == "POST") {
-		loginPostHandler(rw, req)
-	} else {
-		setStatus(rw, req, 405)
+	// Accept a POST endpoint
+	if (req.Method != "POST") {
+		http.Error(rw, errors.New("Method Not Supported").Error(), http.StatusMethodNotAllowed)
+		return
 	}
+	loginPostHandler(rw, req)
 }
 
 func loginPostHandler (rw http.ResponseWriter, req *http.Request) {
-	decoder := json.NewDecoder(req.Body)
-	var loginRequest LoginRequest
-	err := decoder.Decode(&loginRequest)
-	if err != nil {
-		log.Panic(err)
+    var loginRequest LoginRequest
+	
+	// If this was a production environment, we'd hash + salt the password
+	// 	and store in a database so that these passwords aren't stored in
+	//	plaintext. But for now we'll just pretend these are okay.
+
+	validEmail := "c137@onecause.com"
+	validPassword := "#th@nH@rm#y#r!$100%D0p#"
+	// submissionOffeset := 0
+
+    err := json.NewDecoder(req.Body).Decode(&loginRequest)
+    if err != nil {
+        http.Error(rw, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+	// Accept a POST endpoint with a JSON payload with username, password, and token
+	if (len(loginRequest.Email) == 0) {
+		http.Error(rw, errors.New("Required field: Email").Error(), http.StatusBadRequest)
+		return
 	}
-	log.Println(loginRequest)
+
+	if (len(loginRequest.Password) == 0) {
+		http.Error(rw, errors.New("Required field: Password").Error(), http.StatusBadRequest)
+		return
+	}
+
+	if (len(loginRequest.Code) == 0) {
+		http.Error(rw, errors.New("Required field: Code").Error(), http.StatusBadRequest)
+		return
+	}
+
+	if (loginRequest.Email != validEmail || loginRequest.Password != validPassword) {
+		http.Error(rw, errors.New("Invalid username/password combination").Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Success: Redirect to http://onecause.com to signify movement to the next page
+	http.Redirect(rw, req, "http://onecause.com", http.StatusMovedPermanently)
 }
 
 func main() {
-	http.HandleFunc("/auth/login", loginHandler)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/auth/login", loginHandler)
 
-	log.Fatal(http.ListenAndServeTLS(":8080", "/app/cert.pem", "/app/key.pem", nil))
+	log.Fatal(http.ListenAndServeTLS(":8080", "/app/cert.pem", "/app/key.pem", mux))
 }
